7d103241a1247ec56b78e26dfc99f9346030f273
DianaChuga <52463061+DianaChuga@users.noreply.github.com>
2019-07-08 15:13:36 -0500

    Add files via upload

---
+++ Deber #4/Calculo.java
@@ -0,0 +1,13 @@
+public class Calculo{
+	public static void main(String [] args){
+	     System.out.println(factorial(5));
+   	 }
+
+	    public static int factorial(int n) {
+	        int resultado = 1;
+	        for (int i = 1; i <= n; i++) {
+	            resultado *= i;
+        }
+        return resultado;
+    }
+}
---
+++ Deber #4/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial{
+	private int numero ,fac;
+	
+	public Factorial(int numero){
+		this.numero=numero;
+		this.fac =fac;
+	}
+	public void setNumero(){
+		numero=numero;
+	}
+	public int getNumer(){
+		return numero;
+	}
+}
